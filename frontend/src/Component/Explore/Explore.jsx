f9e8be093533b77bf1485cd9f12a7b2e8d1eb416
raj-trivedi <rajtrivedi2255@gmail.com>
2025-06-19 16:25:40 +0530

    Frontend modifications

---
+++ frontend/src/Component/Explore/Explore.jsx
@@ -0,0 +1,55 @@
+import React, { useRef } from 'react';
+import { menu_list } from "../../../../assets/frontend_assets/assets.js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import "./Explore.css";
+
+const Explore = () => {
+    const scrollRef = useRef(null);
+
+    const scroll = (direction) => {
+        const { current } = scrollRef;
+        if (current) {
+            if (direction === 'left') {
+                current.scrollLeft -= 200; // scroll left by 200px
+            } else {
+                current.scrollLeft += 200; // scroll right by 200px
+            }
+        }
+    };
+
+    const [category, setCategory ] = useState("All");
+    return (
+        <div className="explore-container">
+            <h1>Elevate Your Everyday Meal</h1>
+            <p>
+                Crave-worthy dishes made from the freshest ingredients, designed to turn every meal into a moment worth savoring.
+            </p>
+
+            <div className="Explore-slider-container">
+                {/* Left arrow */}
+                <button className="slider-button left" onClick={() => scroll('left')}>
+                    <FontAwesomeIcon icon={faChevronLeft} />
+                </button>
+
+                {/* Scrollable menu list */}
+                <div className="explore-menu-items" ref={scrollRef}>
+                    {menu_list.map((item, index) => (
+                        <div onClick={() => setCategory((prev) => prev===item.menu_name ? "All" : item.menu_name)} className="menu-items" key={index}>
+                            <img src={item.menu_image} alt={item.menu_name} className={category===item.menu_name?"actives":""} />
+                            <p>{item.menu_name}</p>
+                        </div>
+                    ))}
+                </div>
+
+                {/* Right arrow */}
+                <button className="slider-button right" onClick={() => scroll('right')}>
+                    <FontAwesomeIcon icon={faChevronRight} />
+                </button>
+            </div><hr></hr>
+        </div>
+    );
+};
+
+export default Explore;
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import Header from "../Component/Header/Header"
+import Explore from '../Component/Explore/Explore'
+import FoodDisplay from '../Component/FoodDisplay/FoodDisplay'
 
 export const Home = () => {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Explore />
+      <FoodDisplay />
+    </div>
   )
 }
