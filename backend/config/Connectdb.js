77fe0181b413bdec97dad7ec4a241db7bbcb09eb
raj-trivedi <rajtrivedi2255@gmail.com>
2025-07-04 17:00:18 +0530

    fixed myorder page and make address page responsible

--- backend/config/Connectdb.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://raj-trivedi:foodDelivery2205@cluster1.nsarrwv.mongodb.net/food-delivery')
-    .then(() => {
-        console.log("MongoDB connected successfully");
-    })
-}
